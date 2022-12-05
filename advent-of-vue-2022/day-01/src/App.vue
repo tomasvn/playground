1e5eae6c1c349c059fbf1590adb10b3418dec684
Ngoc Tu Nguyen <nguyenngoct2112@gmail.com>
2022-12-05 23:50:25 +0100

    feat: improvement on day 01
    set isLoading prop

--- advent-of-vue-2022/day-01/src/App.vue
+++ advent-of-vue-2022/day-01/src/App.vue
@@ -3,6 +3,7 @@ import { ref, watch } from 'vue'
 
 const searchTerm = ref('')
 const products = ref([])
+const isLoading = ref(false)
 
 function debounce(func, timeout = 500){
   let timer;
@@ -12,8 +13,10 @@ function debounce(func, timeout = 500){
   };
 }
 
-const findProducts = async term => {
+const findProducts = debounce(async term => {
   try {
+    isLoading.value = true;
+
     const res = await fetch(`https://dummyjson.com/products/search?q=${term}&limit=10`);
     const data = await res.json();
   
@@ -21,12 +24,14 @@ const findProducts = async term => {
   
     return products
   } catch(e) {
-    console.error('Error', e.message)
+    console.error('Error', e);
+  } finally {
+    isLoading.value = false
   }
-}
+})
 
 watch(searchTerm, newTerm => {
-  debounce(findProducts(newTerm))
+  findProducts(newTerm)
 });
 </script>
 
@@ -34,8 +39,9 @@ watch(searchTerm, newTerm => {
   <div class="w-full h-full flex flex-col gap-5 justify-center items-center">
     <h1 class="text-4xl font-bold">Gift Search Bar</h1>
     <input type="text" class="p-2 border-2 border-gray-dark" v-model="searchTerm" placeholder="Start typing..." autofocus />
-    <ul class="list-disc">
-      <li v-for="(item, index) in products" :key="index">
+    <p v-if="isLoading">Loading...</p>
+    <ul class="list-disc" v-else-if="(!isLoading && products.length > 0)">
+      <li v-for="(item, index) in products" :key="item.id">
         <h3>{{ item.title }}</h3>
         <span>{{ item.description }}</span>
       </li>
