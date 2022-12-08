ba35483db13c4a2d235be62ecc519ec3e214aa9c
Ngoc Tu Nguyen <nguyenngoct2112@gmail.com>
2022-12-09 00:43:48 +0100

    Day 02 - work in progress

---
+++ advent-of-vue-2022/day-02/src/App.vue
@@ -0,0 +1,17 @@
+<template>
+  <div class="w-full h-full flex justify-center items-center">
+    <span class="text-3xl">Good luck!</span>
+    <span>{{ setupJoke }}</span>
+    <span>{{ deliveryJoke }}</span>
+  </div>
+</template>
+
+<script setup>
+import { computed, ref } from "vue";
+import { useGetJoke } from "./use-get-joke.js";
+
+const { setup, delivery } = useGetJoke();
+
+const setupJoke = computed(() => setup.value)
+const deliveryJoke = computed(() => delivery.value)
+</script>
