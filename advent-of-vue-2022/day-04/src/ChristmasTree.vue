490f9cd82def2104b0b4fedb8072ac927f808ced
Ngoc Tu Nguyen <nguyenngoct2112@gmail.com>
2023-01-05 00:02:35 +0100

    Day 04

---
+++ advent-of-vue-2022/day-04/src/ChristmasTree.vue
@@ -0,0 +1,19 @@
+<template>
+  <div>
+    <div class="flex flex-row justify-center" v-for="i in size">
+      <!-- This div is styled for the tree sections, but it's not complete -->
+      <div v-for="k in i" class="relative rounded-full bg-green w-16 h-16 -m-2 flex justify-center items-center" />
+    </div>
+  </div>
+</template>
+
+<script setup lang="ts">
+withDefaults(
+  defineProps<{
+    size: number
+  }>(),
+  {
+    size: 1,
+  }
+)
+</script>
